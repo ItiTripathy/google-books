167c638886cfa3077015337add5e419a5e697fa5
iti tripathy <iti.tripathy@gmail.com>
2022-07-30 18:27:31 +1000

    updates

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,18 +1,7 @@
 import React from "react";
-import styles from "./BookCard.module.scss";
-import { searchBookDetails } from "./../../scripts/searchBookDetails.js";
 
-const BookCard = ({ book }) => {
-    const details = searchBookDetails(book);
-    return (
-        <>
-            <section className={styles.BookCard}>
-                <img src={details["Thumbnail"]} />
-                <h3>{details["Title"]}</h3>
-                <h4>by {details["Authors"]}</h4>
-            </section>
-        </>
-    );
+const BookCard = () => {
+    return <div>BookCard</div>;
 };
 
 export default BookCard;
